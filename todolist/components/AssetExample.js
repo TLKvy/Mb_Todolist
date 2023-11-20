05293091a527f0ea5ce41b2621ebd521ff81f3bc
TLKvy <151502178+TLKvy@users.noreply.github.com>
2023-11-20 14:12:45 +0700

    Add files via upload

---
+++ todolist/components/AssetExample.js
@@ -0,0 +1,31 @@
+import { Text, View, StyleSheet, Image } from 'react-native';
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        
+        Trần Lâm Khôi Vỹ
+        220501024@student.bdu.edu.vn
+      </Text>
+      <Image style={styles.logo} source={require('../assets/tt.jpg')} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  logo: {
+    height: 168,
+    width: 128,
+  }
+});
